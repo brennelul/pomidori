68037afb9845d2e56f5fe7539f8219b418f698bd
user <brennelul@gmail.com>
2025-03-24 17:32:34 +0700

    commit 2

--- src/classes.py
+++ src/classes.py
@@ -1,8 +1,30 @@
+class Account:
+    def __init__(self, accountID, money):
+        self.__accountID = accountID
+        self.__money = money
+
+    def addMoney(self, amount):
+        if amount > 0:
+            self.__money += amount
+
+    def withdrawMoney(self, amount):
+        if amount > 0:
+            self.__money -= amount
+    
+    def getBalance(self):
+        return self.__money
+    
+    def getAccountID(self):
+        return self.__accountID
+
 class Item:
     def __init__(self, name, color, price):
         self._name = name
         self._price = price
         self._color = color
+
+    def returnItems(self):
+        return self._name, self._color, self._price
 
 class Computers(Item):
     def __init__(self, Processor, iRAM, iROM, name, color, price):
@@ -12,19 +34,19 @@ class Computers(Item):
         self._iROM = iROM
     
     def returnItems(self):
-        return self.__Processor, self.__iRAM, self.__iROM, self.__name, self.__color, self.__price
+        return super().returnItems(), self._Processor, self._iRAM, self._iROM
 
-class Laptop(Computers):
-    def __init__(self, Processor, iRAM, iROM, screenSize, batteryCapacity, name, color, price):
+class Laptops(Computers):
+    def __init__(self, Processor, iRAM, iROM, name, color, price, screenSize, batteryCapacity):
         super().__init__(Processor, iRAM, iROM, name, color, price)
         self._screenSize = screenSize
         self._batteryCapacity = batteryCapacity
+
     def returnItems(self):
-       return self.__Processor, self.__iRAM, self.__iROM, self.__name, self.__color, self.__price
-
-
+        return super().returnItems(), self._screenSize, self._batteryCapacity
+    
 class Smartphones(Item):
-    def __init__(self, Processor, iRAM, iROM, camera, screenSize, batteryCapacity, name, color, price):
+    def __init__(self, Processor, iRAM, iROM, name, color, price, screenSize, batteryCapacity, camera):
         super().__init__(name, color, price)
         self._Processor = Processor
         self._iRAM = iRAM
@@ -32,3 +54,6 @@ class Smartphones(Item):
         self._camera = camera
         self._screenSize = screenSize
         self._batteryCapacity = batteryCapacity
+
+    # def returnItems(self):
+        # return super().returnItems()
--- src/main.py
+++ src/main.py
@@ -12,45 +12,125 @@ def checkInput(maxInt):
     return returnOption
 
 class FSM:
-    __state = 0
-    
+    __state = 99
+    __computers = []
+    __laptops = []
+    __smartphones = []
+    __cart = []
+    __purchaseHistory = []
+
     def __init__(self):
-        with open ("db.json", "r") as file:
+        with open("db.json", "r") as file:
             __data = json.load(file)
-            # print(__data)
-            __computers = []
             for i in __data["Computers"].items():
-                __computers.append(classes.Computers(i[1].get("Processor"),\
+                self.__computers.append(classes.Computers(i[1].get("Processor"),\
                                                      i[1].get("iRAM"), i[1].get("iROM"),\
                                                      i[1].get("name"), i[1].get("color"),\
                                                      i[1].get("price")))
-                print(__computers.)
+            for i in __data["Laptops"].items():
+                self.__laptops.append(classes.Laptops(i[1].get("Processor"),\
+                                                     i[1].get("iRAM"), i[1].get("iROM"),\
+                                                     i[1].get("name"), i[1].get("color"),\
+                                                     i[1].get("price"), i[1].get("screenSize"),\
+                                                     i[1].get("batteryCapacity")))
+            for i in __data["Smartphones"].items():
+                self.__smartphones.append(classes.Smartphones(i[1].get("Processor"),\
+                                                     i[1].get("iRAM"), i[1].get("iROM"),\
+                                                     i[1].get("name"), i[1].get("color"),\
+                                                     i[1].get("price"), i[1].get("screenSize"),\
+                                                     i[1].get("batteryCapacity"), i[1].get("camera")))
+        with open("account.json", "r") as file:
+            __data = json.load(file)
+            self.__account = classes.Account(__data.get("AccountID"), __data.get("Money"))
 
     def FSM_States(self):
-        if self.__state == 0: self.__state = self.menu()
+        if self.__state == 99: self.__state = self.menu()
         elif self.__state == 1: self.__state = self.checkCategories()
-        elif self.__state == 5: return -1
-        # elif self.__state == 2:
+        elif self.__state == 2: self.__state = self.goToCart()
+        elif self.__state == 3: self.__state = self.purchaseHistory()
+        elif self.__state == 4: self.__state = self.checkAccount()
+        elif self.__state == 0: return -1
 
     def menu(self):
-        print("Привет, это DNS\n\t\
-              1) посмотреть категории\n\t\
-              2) перейти в корзину\n\t\
-              3) перейти в историю покупок\n\t\
-              4) посмотреть счет\n\t\
-              5) выход")
-        return checkInput(5)
+        print("Привет, это DNS\n\t"
+              "1) посмотреть категории\n\t"
+              "2) перейти в корзину\n\t"
+              "3) перейти в историю покупок\n\t"
+              "4) посмотреть счет\n\t"
+              "0) выход")
+        return checkInput(4)
 
     def checkCategories(self):
-        # if self.__state != 1: return 1
-        print("Категории:\n\t\
-              1) Компьютеры\n\t\
-              2) Ноутбуки\n\t\
-              3) Смартфоны")
-        self.printCategory(checkInput(3))
+        print("Категории:\n\t"
+              "1) Компьютеры\n\t"
+              "2) Ноутбуки\n\t"
+              "3) Смартфоны")
+        return self.printCategory(checkInput(3))
     
-    # def printCategory(option):
+    def printCategory(self, option):
+        if option == 1:
+            self.printData(self.__computers)
+            return self.addToCart(checkInput(len(self.__computers)), self.__computers)
+        elif option == 2:
+            self.printData(self.__laptops)
+            return self.addToCart(checkInput(len(self.__laptops)), self.__laptops)
+        elif option == 3:
+            self.printData(self.__smartphones)
+            return self.addToCart(checkInput(len(self.__smartphones)), self.__smartphones)
 
+        return 99
+    
+    def printData(self, data):
+        for i in range(len(data)):
+            print(f"{i + 1}. {data[i]._name}: Процессор: {data[i]._Processor}, ОЗУ: {data[i]._iRAM} GB, Накопитель: {data[i]._iROM} GB\n"
+                    f"Цена: {data[i]._price} руб.")
+        print("0. Выход")
+
+    def addToCart(self, option, data):
+        print(option)
+        if option > 0 and option <= len(data):
+            self.__cart.append(data[option - 1]) 
+        elif option > len(data):
+            print("Некорректное значение")
+        
+        return 99
+        
+    def goToCart(self):
+        if len(self.__cart) > 0:
+            total = 0
+            for i in range (len(self.__cart)):
+                print(f"{i + 1}. {self.__cart[i]._name}, Цена: {self.__cart[i]._price} руб.")
+                total += self.__cart[i]._price
+
+            print(f"Всего: {total} руб.")
+            print("\t1) Купить\n\t"
+              "2) Очистить корзину\n\t"
+              "0) Выход")
+            
+            option = checkInput(2)
+            if option == 1: self.buyCart(total)
+            elif option == 2: self.__cart.clear()
+        
+        else: print("Пусто!")
+        return 99
+    
+    def buyCart(self, total):
+        if self.__account.getBalance() >= total:
+            self.__account.withdrawMoney(total)
+            self.__purchaseHistory.append(self.__cart)
+            self.__cart.clear()
+        else:
+            print("Ошибка, недостаточно средств")
+    
+    def purchaseHistory(self):
+        # print("not implemented")
+        for i in range (len(self.__cart)):
+            print(f"{i}. {self.__purchaseHistory[i]._name}")
+        return 99 
+    
+    def checkAccount(self):
+        print("not implemented")
+        return 99
 
 def main():
     store = FSM()
